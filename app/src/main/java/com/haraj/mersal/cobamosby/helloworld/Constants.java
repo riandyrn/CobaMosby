ace1169101e60f0d7ea1211f71e4eaa0109fb0fb
Riandy Rahman Nugraha <riandyrn@gmail.com>
2016-04-09 09:06:23 +0700

    test

--- app/src/main/java/com/haraj/mersal/cobamosby/helloworld/Constants.java
+++ app/src/main/java/com/haraj/mersal/cobamosby/helloworld/Constants.java
@@ -4,6 +4,7 @@ package com.haraj.mersal.cobamosby.helloworld;
  * Created by riandyrn on 4/9/16.
  */
 public class Constants {
+    // Hello World!
     public final static String IDENTITY_POOL_ID = "YOUR_IDENTITY_POOL_ID";
     public final static String BUCKET_NAME = "cobamosby";
     public final static String BASE_URL = "https://cobamosby.s3-eu-west-1.amazonaws.com/";
